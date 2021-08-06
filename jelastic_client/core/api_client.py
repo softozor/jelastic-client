47a7102fc508b394e2ffdcb43cbd4f9a0fda3d91
Laurent Michel <softozor@gmail.com>
2021-08-06 14:17:09 +0200

    Urljoin uris + merge publication to pypi registries in the same job

--- jelastic_client/core/api_client.py
+++ jelastic_client/core/api_client.py
@@ -4,6 +4,7 @@ from typing import Dict
 import httpx
 
 from .exceptions import ApiClientException
+from urllib.parse import urljoin
 
 
 class ApiClient:
@@ -24,8 +25,7 @@ class ApiClient:
         self.logger.debug(f"_apicall {method.upper()} {uri}, data:{data}")
         data.update(self.api_data)
         r = self.client.request(
-            # TODO: use some kind of urljoin here to avoid problems with slashes
-            method=method, url=f"{self.api_url}/{uri}", data=data
+            method=method, url=urljoin(self.api_url, uri), data=data
         )
         if r.status_code != httpx.codes.OK:
             raise ApiClientException(
