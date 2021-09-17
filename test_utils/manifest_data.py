d621af4430540cc465870c499f18a51f791c7d69
Laurent Michel <softozor@gmail.com>
2021-09-17 09:55:41 +0200

    fix get_manifest_data so we can grab urls out of the success text

--- test_utils/manifest_data.py
+++ test_utils/manifest_data.py
@@ -4,7 +4,12 @@ def get_manifest_data(success_text):
 
     manifest_data = {}
     for key_value in success_text.split('<br />\n'):
-        key, value = key_value.split(':')
+        split_item = key_value.split(':')
+        key = split_item[0]
+        value = split_item[1]
+        # to make it more flexible we could use re.sub
+        # and try to extract code contained within optional
+        # html tags
         key = key.replace('<strong>', '')
         key = key.replace('</strong>', '')
         manifest_data[key] = value.strip()
