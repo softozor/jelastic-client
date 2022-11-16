5e1bf413439b42c58b535a23e8f34a319994b0cd
Laurent Michel <laurent.michel@cognex.com>
2022-11-16 12:02:04 +0100

    hopefully fix branch filter

--- .teamcity/integrationConfig/Integration.kt
+++ .teamcity/integrationConfig/Integration.kt
@@ -28,6 +28,7 @@ class Integration(
         vcs {
             branchFilter = """
                 +:*
+                -:v*
             """.trimIndent()
             triggerRules = """
                 +:jelastic_client/**
