9ce186d3dd0da97d5d2e69d46739f5d37d341a79
Laurent Michel <laurent.michel@cognex.com>
2023-02-04 17:14:59 +0100

    fix test

--- test/test_manifest_data.py
+++ test/test_manifest_data.py
@@ -9,13 +9,11 @@ from jelastic_client import get_manifest_data
     "properly_formatted_success_text,expected_dict",
     [
         (
-            """<strong>field1</strong>: value1<br />
-<strong>field2</strong>: value2""",
+            "<strong>field1</strong>: value1<br />\n<strong>field2</strong>: value2",
             {"field1": "value1", "field2": "value2"},
         ),
         (
-            """field1: value1<br />
-field2: value2""",
+            "field1: value1<br />\nfield2: value2",
             {"field1": "value1", "field2": "value2"},
         ),
         ("", {}),
@@ -37,10 +35,8 @@ def test_get_manifest_data_from_properly_formatted_success_text_returns_expected
     "improperly_formatted_success_text,expected_dict",
     [
         (
-            """<i>field1</i>: value1<br />
-        <p>field2: value2
-    """,
-            {"<i>field1</i>": "value1", "        <p>field2": "value2"},
+            "<i>field1</i>: value1<br />\n<p>field2: value2",
+            {"<i>field1</i>": "value1", "<p>field2": "value2"},
         ),
     ],
 )
@@ -48,14 +44,9 @@ def test_get_manifest_data_from_improperly_formatted_text_returns_improper_dict(
     improperly_formatted_success_text: str, expected_dict: Dict[str, str]
 ):
     # Arrange
-    properly_formatted_success_text = """<i>field1</i>: value1<br />
-        <p>field2: value2
-    """
-
-    expected_dict = {"<i>field1</i>": "value1", "        <p>field2": "value2"}
 
     # Act
-    actual_dict = get_manifest_data(properly_formatted_success_text)
+    actual_dict = get_manifest_data(improperly_formatted_success_text)
 
     # Assert
     assert actual_dict == expected_dict
